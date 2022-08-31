29b314056bcc0354519da493de067e589a7d36a0
Grant Schowalter <grantschowalter@gmail.com>
2022-08-31 14:48:56 -0700

    carousel kind of working

---
+++ components/gallery.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import { PrevButton, NextButton } from "./EmblaCarouselButtons";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+import { mediaByIndex } from "./media";
+import styles from "./gallery.module.css";
+
+const EmblaCarousel = ({ slides, options = { loop: false } }) => {
+  const autoplay = useRef(
+    Autoplay(
+      { delay: 3000, stopOnInteraction: false },
+      (emblaRoot) => emblaRoot.parentElement
+    )
+  );
+
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [autoplay.current]);
+  const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
+  const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
+
+  const scrollNext = useCallback(() => {
+    if (!emblaApi) return;
+    emblaApi.scrollNext();
+    autoplay.current.reset();
+  }, [emblaApi]);
+
+  const scrollPrev = useCallback(() => {
+    if (!emblaApi) return;
+    emblaApi.scrollPrev();
+    autoplay.current.reset();
+  }, [emblaApi]);
+
+  const onSelect = useCallback(() => {
+    if (!emblaApi) return;
+    setPrevBtnEnabled(emblaApi.canScrollPrev());
+    setNextBtnEnabled(emblaApi.canScrollNext());
+  }, [emblaApi]);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+    onSelect();
+    emblaApi.on("select", onSelect);
+  }, [emblaApi, onSelect]);
+
+  return (
+    <div className={styles.embla}>
+      <div className={styles.embla__viewport} ref={emblaRef}>
+        <div className={styles.embla__container}>
+          {slides.map((index) => (
+            <div className={styles.embla__slide} key={index}>
+              <div className={styles.embla__slide__inner}>
+                <img
+                  className={styles.embla__slide__img}
+                  src={mediaByIndex(index).src}
+                  alt="movie"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <PrevButton onClick={scrollPrev} enabled={prevBtnEnabled} />
+      <NextButton onClick={scrollNext} enabled={nextBtnEnabled} />
+    </div>
+  );
+};
+
+export default EmblaCarousel;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import Gallery from "../components/gallery";
+
+const SLIDE_COUNT = 5;
+const slides = Array.from(Array(SLIDE_COUNT).keys());
 
 export async function getStaticProps() {
   const allPostsData = await getSortedPostsData();
@@ -15,32 +19,33 @@ export async function getStaticProps() {
 
 export default function Home({ allPostsData }) {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          &emsp;&emsp; Saving your time in our fast paced world. Let us navigate
-          the ocean of content out there today so you can save time watching TV
-          and Movies that are worth watching. We take our time so you don't have
-          to!
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Reviews</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, review_date, title }) => (
-            <li className={utilStyles.listItem} key={title + review_date}>
-              <Link href={`/posts/${title.replace(/\s/g, "")}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small>{review_date}</small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+    // <Layout home>
+    //   <Head>
+    //     <title>{siteTitle}</title>
+    //   </Head>
+    //   <section className={utilStyles.headingMd}>
+    //     <p>
+    //       &emsp;&emsp; Saving your time in our fast paced world. Let us navigate
+    //       the ocean of content out there today so you can save time watching TV
+    //       and Movies that are worth watching. We take our time so you don't have
+    //       to!
+    //     </p>
+    //   </section>
+    //   <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+    //     <h2 className={utilStyles.headingLg}>Reviews</h2>
+    //     <ul className={utilStyles.list}>
+    //       {allPostsData.map(({ id, review_date, title }) => (
+    //         <li className={utilStyles.listItem} key={title + review_date}>
+    //           <Link href={`/posts/${title.replace(/\s/g, "")}`}>
+    //             <a>{title}</a>
+    //           </Link>
+    //           <br />
+    //           <small>{review_date}</small>
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </section>
+    // </Layout>
+    <Gallery slides={slides} />
   );
 }
