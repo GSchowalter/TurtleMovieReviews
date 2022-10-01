c3a2c553e225738ac40344063339224aef6dfd8d
Grant Schowalter <grantschowalter@gmail.com>
2022-10-01 12:07:18 -0700

    Updated layout. Basic about us page is up

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -1,17 +1,23 @@
 import Layout from "../components/layout";
 import Image from "next/image";
 
-export const meettheteam = () => {
+export const aboutus = () => {
   return (
     <Layout>
-      <Image
-        src="/images/about_us/header.svg"
-        width="1600"
-        height="900"
-        layout="responsive"
-      ></Image>
+      <div>
+        <h1>About Us</h1>
+      </div>
+      <div>
+        <h2>Our Mission</h2>
+      </div>
+      <div>
+        <h2>Socials</h2>
+      </div>
+      <div>
+        <h2>Contact Us</h2>
+      </div>
     </Layout>
   );
 };
 
-export default meettheteam;
+export default aboutus;
